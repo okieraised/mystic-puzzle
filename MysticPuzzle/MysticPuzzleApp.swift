6856a378bcc0098ff21d4165ba219e3dfdac6b73
okieraised <tripg@vingroup.net>
2023-02-23 19:41:03 +0700

    Initial Commit

---
+++ MysticPuzzle/MysticPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MysticPuzzleApp.swift
+//  MysticPuzzle
+//
+//  Created by Tri Pham on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MysticPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
